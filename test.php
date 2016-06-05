d060df520ce9f2bc17e2132f82841f2456d38d56
ritesh96 <riteshhota.2008@gmail.com>
2016-06-05 23:11:40 +0530

    Added test questions

---
+++ test.php
@@ -0,0 +1,105 @@
+<?php 
+    require("config.php");
+    if(!empty($_POST)) 
+    { 
+        // Ensure that the user fills out fields 
+        if(empty($_POST['answer'])) 
+        { die("Please enter an answer."); } 
+        
+        // Check if the username is already taken
+        $query = " 
+            SELECT 
+                1 
+            FROM users 
+            WHERE 
+                username = :username 
+        "; 
+        $query_params = array( ':username' => $_POST['username'] ); 
+        try { 
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params); 
+        } 
+        catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
+        $row = $stmt->fetch(); 
+        if($row){ die("This username is already in use"); } 
+        $query = " 
+            SELECT 
+                1 
+            FROM users 
+            WHERE 
+                email = :email 
+        "; 
+        $query_params = array( 
+            ':email' => $_POST['email'] 
+        ); 
+        try { 
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params); 
+        } 
+        catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage());} 
+        $row = $stmt->fetch(); 
+        if($row){ die("This email address is already registered"); } 
+         
+        // Add row to database 
+        $query = " 
+            INSERT INTO users ( 
+                username, 
+                password, 
+                salt, 
+                email 
+            ) VALUES ( 
+                :username, 
+                :password, 
+                :salt, 
+                :email 
+            ) 
+        "; 
+         
+        // Security measures
+        $salt = dechex(mt_rand(0, 2147483647)) . dechex(mt_rand(0, 2147483647)); 
+        $password = hash('sha256', $_POST['password'] . $salt); 
+        for($round = 0; $round < 65536; $round++){ $password = hash('sha256', $password . $salt); } 
+        $query_params = array( 
+            ':username' => $_POST['username'], 
+            ':password' => $password, 
+            ':salt' => $salt, 
+            ':email' => $_POST['email'] 
+        ); 
+        try {  
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params); 
+        } 
+        catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
+        header("Location: index.php"); 
+        die("Redirecting to index.php"); 
+    } 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+</head>
+<body>
+
+<div class="row">
+    <form class="col s12" action="" method="post">
+     	<div class="row">
+       		<div class="input-field col s6">
+        		<input placeholder="Placeholder" id="first_name" name="answer" type="text" class="validate">
+          			<label for="first_name">First Name</label>
+    		</div>
+      	</div>
+      	<button class="btn waves-effect waves-light" type="submit" name="action">Submit
+    <i class="material-icons right">send</i>
+  </button>
+    </form>
+</div>
+
+</body>
+</html>
