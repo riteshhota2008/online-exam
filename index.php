33b0fe9951c0d71a4b7b3597813db2c16f1c4c71
ritesh96 <riteshhota.2008@gmail.com>
2016-06-11 18:19:55 +0530

    Updated homepage and login layout

--- index.php
+++ index.php
@@ -6,23 +6,19 @@ session_start();
 
 ?>
 <!DOCTYPE html>
-<html lang="en-us" itemscope itemtype="http://schema.org/Article">
+<html lang="en-us">
 
 <head>
 
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-	<link rel="stylesheet" type="text/css" href="css/style.css" >
+<!--	<link rel="stylesheet" type="text/css" href="css/style.css" > -->
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
 
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 	<title>Login and registration</title>
 </head>
@@ -38,14 +34,14 @@ session_start();
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Brand</a>
+            <a class="navbar-brand" href="#">Ritesh</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Link</a></li>
+                <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+                <li><a href="register.php">Register</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
                     <ul class="dropdown-menu">
@@ -66,7 +62,7 @@ session_start();
                 <button type="submit" class="btn btn-default">Submit</button>
             </form>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Link</a></li>
+                <li><a href="login.php">Login</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
                     <ul class="dropdown-menu">
@@ -82,16 +78,23 @@ session_start();
     </div><!-- /.container-fluid -->
 </nav>
 
-	<div id="container">
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="register.php">Register</a></li>
-			<li><a href="login.php">Login</a></li>
-		</ul>
-		<h1>Welcome to Ritesh's site!</h1>
+<div class="container">
+    <div class="jumbotron" style="margin-top: 20px">
+        <h1>Welcome to Online Exam Portal!</h1><br>
+       <!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>-->
+    </div>
+</div>
 		<?php if (isset($_GET['emailsent']) && empty($_GET['emailsent'])) {
 			echo '<h3>Please check your email for password reset link</h3>';
 		}?>
-	</div>
+
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+
 </body>
 </html>
--- login.php
+++ login.php
@@ -32,22 +32,85 @@ if (empty($_POST) === false) {
 	}
 }
 ?>
-<!doctype html>
+<!DOCTYPE html>
+<html lang="en-us">
 
 <head>
-	
-	<link rel="stylesheet" type="text/css" href="css/style.css" >
-	<title>Login</title>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+	<!--	<link rel="stylesheet" type="text/css" href="css/style.css" > -->
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css" >
+
+
+
+	<title>Login and registration</title>
 </head>
-<body>	
-	<div id="container">
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="register.php">Register</a></li>
-			<li><a href="login.php">Login</a></li>
-			<li><a href="forgotpass.php">Forgot Password</a></li>
-		</ul>
-		<h1>Login</h1>
+<body>
+
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">Ritesh</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+				<li><a href="register.php">Register</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Something else here</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="#">Separated link</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="#">One more separated link</a></li>
+					</ul>
+				</li>
+			</ul>
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="login.php">Login</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="profile.php">Profile</a></li>
+						<li><a href="forgotpass.php">Forgot Password</a></li>
+						<li><a href="logout.php">Log out</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="edit.php">Edit Profile</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+</nav>
+
+<div class="container">
+	<div class="jumbotron" style="margin-top: 20px">
+		<h1>Welcome to Online Exam Portal!</h1><br>
+		<!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>-->
+	</div>
+
  
 		<?php if(empty($errors) === false){
  
@@ -64,6 +127,14 @@ if (empty($_POST) === false) {
 			<input type="checkbox" name="loggedin" value="true">&nbsp keep me logged in</br>
 			<input type="submit" name="submit">
 		</form>
-	</div>
+</div>
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+
 </body>
 </html>
