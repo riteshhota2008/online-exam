9a4e84f4c3a8706cbaa52f56193c2e366338ca1b
Ritesh Hota <riteshhota.2008@gmail.com>
2016-12-09 10:47:11 +0530

    Updated Index and Registration page

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-<?php 
+<?php
 
 require 'core/init.php';
 session_start();
-
 
 ?>
 <!DOCTYPE html>
@@ -10,17 +9,41 @@ session_start();
 
 <head>
 
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-<!--	<link rel="stylesheet" type="text/css" href="css/style.css" > -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    <!--	<link rel="stylesheet" type="text/css" href="css/style.css" > -->
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600" rel="stylesheet">
+    <style>
+        h1, h2, h3, h4, h5, h6, p, a, li, ul, label, input, span {
+            font-family: 'Source Sans Pro', sans-serif;
+            font-weight: 400;
+        }
+
+        /* Sticky footer styles
+        -------------------------------------------------- */
+        html {
+            position: relative;
+            min-height: 100%;
+        }
+
+        .footer {
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+            /* Set the fixed height of the footer here */
+            height: 60px;
+            background-color: #333;
+            padding-top: 20px;
+        }
+
+    </style>
 
 
-
-	<title>Login and registration</title>
+    <title>Login and registration</title>
 </head>
 <body>
 
@@ -28,51 +51,24 @@ session_start();
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Ritesh</a>
+            <a class="navbar-brand" href="#">Exam Portal</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
-                <!--<li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li>-->
             </ul>
-            <!--<form class="navbar-form navbar-left" role="search">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search">
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>-->
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="login.php">Login</a></li>
                 <li><a href="register.php">Register</a></li>
-                <!--<li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                    </ul>
-                </li>-->
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
@@ -80,13 +76,23 @@ session_start();
 
 <div class="container">
     <div class="jumbotron" style="margin-top: 20px">
-        <h1>Welcome to Online Exam Portal!</h1><br>
-       <!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>-->
+        <h2>Welcome to Online Exam Portal!</h2><br>
+        <h2>Click on register or login to proceed to exam portal.</h2>
+        <!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>-->
     </div>
 </div>
-		<?php if (isset($_GET['emailsent']) && empty($_GET['emailsent'])) {
-			echo '<h3 style="margin-left: 70px">Please check your email for password reset link.</h3>';
-		}?>
+
+<footer class="footer" style="margin-top: 45px">
+    <div class="container">
+        <p class="text-muted" style="color: #ffffff;">Copyright &copy; <a href="http://riteshhota2008.github.io"
+                                                                          style="color: gold">Ritesh Hota</a>. All
+            Rights Reserved.</p>
+    </div>
+</footer>
+
+<?php if (isset($_GET['emailsent']) && empty($_GET['emailsent'])) {
+    echo '<h3 style="margin-left: 70px">Please check your email for password reset link.</h3>';
+} ?>
 
 
 <!-- jQuery -->
--- register.php
+++ register.php
@@ -1,245 +1,147 @@
-<?php 
+<?php
 require('core/init.php');
 if (isset($_POST['submit'])) {
 
-	if(empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])||empty($_POST['confirm_password'])){
+    if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email']) || empty($_POST['confirm_password'])) {
 
-		$errors[] = 'All fields are required.';
+        $errors[] = 'All fields are required.';
 
-	}else{
-
-        
-		if ($users->user_exists($_POST['username']) === true) {
-		 	$errors[] = 'That username already exists';
-		}
-		if (!preg_match("/[a-zA-Z0-9-_]+/",$_POST['username'])){
-			$errors[] = 'The user name should be alphanumeric characters with some special symbols (-,_).';
-		}
-		if (strlen($_POST['password']) <6){
-			$errors[] = 'Your password must be at least 6 characters';
-		} else if (strlen($_POST['password']) >12){
-			$errors[] = 'Your password cannot be more than 12 characters long';
-		}
-		if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
-			$errors[] = 'Please enter a valid email address';
-		}else if ($users->email_exists($_POST['email']) === true) {
-		 	$errors[] = 'That email already exists.';
-		}
-		
-		if($_POST['password']!=$_POST['confirm_password']){
-			$errors[]='passwords don\'t match';
-		}
-		if ($_POST['tc']!="1"){
-			$errors[]='Please accept the terms and condition';
-		}
+    } else {
 
 
-	}
+        if ($users->user_exists($_POST['username']) === true) {
+            $errors[] = 'That username already exists';
+        }
+        if (!preg_match("/[a-zA-Z0-9-_]+/", $_POST['username'])) {
+            $errors[] = 'The user name should be alphanumeric characters with some special symbols (-,_).';
+        }
+        if (strlen($_POST['password']) < 6) {
+            $errors[] = 'Your password must be at least 6 characters';
+        } else if (strlen($_POST['password']) > 12) {
+            $errors[] = 'Your password cannot be more than 12 characters long';
+        }
+        if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+            $errors[] = 'Please enter a valid email address';
+        } else if ($users->email_exists($_POST['email']) === true) {
+            $errors[] = 'That email already exists.';
+        }
 
-	if(empty($errors) === true){
-		
-		$username 	= htmlentities($_POST['username']);
-		$password 	= $_POST['password'];
-		$email 		= htmlentities($_POST['email']);
+        if ($_POST['password'] != $_POST['confirm_password']) {
+            $errors[] = 'passwords don\'t match';
+        }
+        if ($_POST['tc'] != "1") {
+            $errors[] = 'Please accept the terms and condition';
+        }
 
-		$users->register($username, $password, $email);
-		header('Location: register.php?success');
-		exit();
-	}
+
+    }
+
+    if (empty($errors) === true) {
+
+        $username = htmlentities($_POST['username']);
+        $password = $_POST['password'];
+        $email = htmlentities($_POST['email']);
+
+        $users->register($username, $password, $email);
+        header('Location: register.php?success');
+        exit();
+    }
 }
 
 if (isset($_GET['success']) && empty($_GET['success'])) {
-	echo 'Thank you for registering. Please check your email.';
+    echo 'Thank you for registering. Please check your email.';
 }
 ?>
 
 <html>
 <head>
 
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="css/bootstrap.min.css" >
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 
-	<style>
+    <style>
+        /* Box Card Design */
+        .box-material {
+            padding: 30px;
+            background-color: white;
+            box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.04);
+            border-radius: 5px;
+            border: 1px solid #e5e5e5;
+        }
+    </style>
 
-		body{
-			background: url('images/GSC.png') no-repeat top center;
-			background-size: 50%;
-			background-position: 1% 30%;
-			background-position-y: 50px;
-		}
-
-		@font-face{
-
-		}
-
-		.container {
-			margin-left: 58%;
-			width: 41%;
-			margin-top: 40px;
-			height: 569px;
-			border-left: 1px solid #d6d6d6;
-		}
-
-		.container2{
-			width: 80%;
-			float: right;
-			margin-top: 150px;
-		}
-
-		/*input{
-            margin-top:8px;
-        }*/
-
-
-		h1{
-			/*font-family: 'Segoe UI Light';*/
-			font-weight: 200;
-			font-style: normal;
-			font-size: 2.15em;
-		}
-
-		.h1_pos{
-			margin-top:-50px;
-		}
-
-		.about_pos {
-			margin-top: 200px;
-			margin-left: 180px;
-		}
-
-		.panel {
-			-webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
-			box-shadow: 0 1px 2px rgba(0,0,0,.05);
-			-webkit-box-shadow: 0px -1px 20px 0px rgba(50, 50, 50, 0.48);
-			-moz-box-shadow: 0px -1px 20px 0px rgba(50, 50, 50, 0.48);
-			box-shadow: 0px -1px 20px 0px rgba(50, 50, 50, 0.48);
-		}
-
-	</style>
-
-	<title>Register</title>
+    <title>Register</title>
 </head>
-<body>
+<body style="background-color: #f9f9f9">
 
 
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-inverse" style="border-radius: 0px">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Ritesh</a>
+            <a class="navbar-brand" href="#">Exam Portal</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
-                <!--<li><a href="register.php">Register</a></li>-->
-                <!--<li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li>-->
             </ul>
-            <!--<form class="navbar-form navbar-left" role="search">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search">
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>-->
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="login.php">Login</a></li>
-                <!--<li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="profile.php">Profile</a></li>
-                        <li><a href="forgotpass.php">Forgot Password</a></li>
-                        <li><a href="logout.php">Log out</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="edit.php">Edit Profile</a></li>
-                    </ul>
-                </li>-->
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
 
 
-
-<div class="container">
-    <div class="container2" style="padding-right: 50px;">
-        <div class="h1_pos">
-            <h1>Register for Exam Portal!</h1>
-        </div><br><br><br>
-        <form method="post" action="">
-            <input type="text" class="form-control" name="username" placeholder="Username" title="Enter username here" /><br>
-            <input type="email" class="form-control" name="email" placeholder="Email" title="Enter email here" /><br>
-            <input type="password" class="form-control" name="password" placeholder="Password" title="Enter password here" /><br>
-            <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" title="Enter password again" /><br>
-            <input type="checkbox" class="form-control" name="tc" value="1" />"I agree terms and condition"
-            <input type="submit" class="btn btn-default" name="submit" value="Sign up" style="float: right;"/>
-            <div class="about_pos">
-                <!-- <a href="AboutManagement.php" style="text-decoration:none; color: silver">About management</a> -->
+<div class="container" style="margin-top: 50px">
+    <div class="row">
+        <div class="col-md-offset-4 col-md-4 col-md-offset-4">
+            <div class="box-material">
+                <h2>Registration.</h2>
+                <form method="post" action="">
+                    <input type="text" class="form-control" name="username" placeholder="Username"
+                           title="Enter username here"/><br>
+                    <input type="email" class="form-control" name="email" placeholder="Email" title="Enter email here"/><br>
+                    <input type="password" class="form-control" name="password" placeholder="Password"
+                           title="Enter password here"/><br>
+                    <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password"
+                           title="Enter password again"/><br>
+                    <input type="checkbox" class="form-control" name="tc" value="1"/><label>"I agree terms and
+                        condition"</label><br><br>
+                    <input type="submit" class="btn btn-success btn-block" name="submit" value="Sign up"/>
+                </form>
             </div>
-        </form>
+        </div>
     </div>
 </div>
 
-<!--	<div id="container">
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="register.php">Register</a></li>
-			<li><a href="login.php">Login</a></li>
-		</ul>
-		<h1>Register</h1>
 
-		<form method="post" action="">
-			<h4>Username:</h4>
-			<input type="text" name="username" />
-			<h4>Email:</h4>
-			<input type="text" name="email" />	
-			<h4>Password:</h4>
-			<input type="password" name="password" />
-			<h4>Confirm Password:</h4>
-			<input type="password" name="confirm_password" />
-			<h4>Terms & Condition:</h4>
-			<input type="checkbox" name="tc" value="1" />"I agree terms and condition"	
-			
+<?php
 
-			<br>
-			<input type="submit" name="submit" />
-		</form>
--->
+if (empty($errors) === false) {
+    echo '<p>' . implode('</p><p>', $errors) . '</p>';
+}
 
-		<?php 
-		
-		if(empty($errors) === false){
-			echo '<p>' . implode('</p><p>', $errors) . '</p>';
-		}
-
-		?>
+?>
 <!--
 	</div>
 
 -->
 
-	<!-- jQuery -->
-	<script src="js/jquery.js"></script>
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
 
-	<!-- Bootstrap Core JavaScript -->
-	<script src="js/bootstrap.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
 
 </body>
 </html>
