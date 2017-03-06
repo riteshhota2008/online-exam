27be6a193667b02805868278c8c52b3637e61ca3
Ritesh Hota <riteshhota.2008@gmail.com>
2017-03-06 18:54:48 +0530

    Update database.php

--- core/connect/database.php
+++ core/connect/database.php
@@ -3,9 +3,9 @@
 
 $config = array(
 	'host' => 'localhost',
-	'username'=>'u372998677_stud',
-	'password'=> 'abhisekh96',
-	'dbname'=>'u372998677_stud'
+	'username'=>'root',
+	'password'=> 'pass',
+	'dbname'=>'db'
 	);
 
 
